26cb74a641d91660a33b54588614981dd652d0bc
geunjun <junseo4580@naver.com>
2025-09-09 11:15:00 +0900

    Add files via upload

---
+++ kjs java/ch00/src/ch00/char1.java
@@ -0,0 +1,11 @@
+package ch001;
+
+public class ch0001 {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+	}
+
+}
---
+++ kjs java/ch01/src/main/java/ch01/char1.java
@@ -0,0 +1,15 @@
+package ch01;
+
+public class char1 {
+	byte byteVal = 10;
+	int intVal = byteVal;
+	//byte byteVal = intVal;\
+	
+	char charVal = 65;
+	//char charVal = 'A';
+	//int intVall = charVal;
+	//byte byteVall = 65;
+	//char charVal2 = byteVall;
+	System.out.println(result);
+}
+
