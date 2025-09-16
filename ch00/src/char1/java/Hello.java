31ce3143518c9688ab3b1c2e7288d62ae99a0785
geunjun <junseo4580@naver.com>
2025-09-16 16:45:09 +0900

    Create Hello.java

---
+++ ch00/src/char1/java/Hello.java
@@ -0,0 +1,12 @@
+package char1.java;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      int x = 1;
+	      int y = 2;
+	      int result = x + y;
+	}
+
+}
